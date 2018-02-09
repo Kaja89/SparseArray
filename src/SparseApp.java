28bd1d5f0d8d5db3b6b4993de052d7aff9f541c5
Kaja89 <kajaspiro@gmail.com>
2018-02-09 12:15:29 +0100

    some improvements

--- src/SparseApp.java
+++ src/SparseApp.java
@@ -1,24 +1,23 @@
 import java.util.AbstractList;
-import java.util.Arrays;
 
 class SparseList<T> extends AbstractList<T>{
 
-	//AbstractList<T> sparse;
 	Object sparse[][];
 	
 	public SparseList(){
-		sparse = new Object [5][5];
+		sparse = new Object [1000][2];
 	}
 	
-	
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public T get(int index) {
-		for(int i = 0; i<sparse.length; i++) {
-			if(sparse[index][i] != null) {
-				return (T) sparse[index][i];
-			}
+		if((T) sparse[index][1] == null) {
+			System.out.println("This index is empty!");
+			return null;
+		} else {
+			return (T) sparse[index][1];
 		}
-		return null;
 	}
 
 	@Override
@@ -28,14 +27,12 @@ class SparseList<T> extends AbstractList<T>{
 	
 	@Override
     public void add(int index, T element) {
-		/*if(index >= 5) {
-			int newSize = sparse.length*2;
-			sparse = Arrays.copyOf(sparse, newSize);
-		}*/
+		
 		for(int i = 0; i<sparse.length; i++) {
 			
-			if(sparse[index][i] == null) {
-				sparse[index][i] = element;
+			if(sparse[index][0] == null) {
+				sparse[i][0] = index;
+				sparse[index][1] = element;
 				break;
 			}
 		}
@@ -50,8 +47,10 @@ public class SparseApp {
 		theList.add(4, "Adam");
 		
 		System.out.println("The list size: " + theList.size());
-		System.out.println("The list element 4: " + theList.get(4));
+		System.out.println("The list element 2: " + theList.get(2));
 		System.out.println("The list element 3: " + theList.get(3));
+		System.out.println("The list element 4: " + theList.get(4));
+		
 	}
 
 }
